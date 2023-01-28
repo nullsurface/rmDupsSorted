069196dc156e78b948ce7eb29bc5373f78d91992
andrew <andrew@node.rip>
2023-01-28 23:38:02 +0000

    last

--- src/main.c
+++ src/main.c
@@ -1,12 +1,45 @@
+#include <stdio.h>
 int removeDuplicates(int*, int);
+void printList(int*, int);
 
 int main() {
+    int arr1[3] = {1,1,2};
+    int arr2[10] = {0,0,1,1,1,2,2,3,3,4};
+
+    printf("%d\n", removeDuplicates(arr1, 3));
+    printf("%d\n", removeDuplicates(arr2, 10));
+    printList(arr1, 3);
+    printList(arr2, 10);
 
     return 0;
 }
 
 int removeDuplicates(int* nums, int numsSize) {
-    
-    return 0;
+    int spaceSize = 0;
+    int lastNum = nums[0];
+
+    for (int i = 1; i < numsSize; i++) {
+	if (spaceSize > 0)
+	    nums[i - spaceSize] = nums[i];
+
+	if (nums[i] == lastNum) {
+	    nums[i] = -101;
+	    spaceSize++;
+	} else {
+	    lastNum = nums[i];
+	}
+
+    }
+
+    return numsSize - spaceSize;
+}
+
+void printList(int* list, int size) {
+    printf("[");
+
+    for (int i = 0; i < size; i++)
+	printf("%d ", list[i]);
+
+    printf("]\n");
 }
 
